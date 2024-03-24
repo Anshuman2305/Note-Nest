24db54e3c8931b8b81729b557f337137cee89a52
Anshuman2305 <anshuman.nitrr@gmail.com>
2024-03-24 14:42:13 +0530

    UI Changes

--- components/Nav.js
+++ components/Nav.js
@@ -24,9 +24,9 @@ function Nav() {
     return (
         <div className="w-[100%]">
         <Slidereveal>
-        <div className="flex flex-row justify-between w-[100%] items-center mt-4 sm:mt-8 px-4 lg:px-12">
+        <div className="flex flex-row justify-between w-[100%] items-center mt-4 sm:mt-4 px-4 lg:px-14">
             <div>
-                <p className="sm:text-3xl text-[#1a2838] p-3 border-[1px] border-[#b4c7db] rounded-2xl bg-[#D4E8FD]">NN</p>
+                <p className="sm:text-2xl text-[#1a2838] p-3 border-[1px] border-[#b4c7db] rounded-2xl bg-[#D4E8FD]">NN</p>
             </div>
             <div className="flex flex-row gap-4">
                 <a href="https://github.com/Anshuman2305" target="_blank" className="px-4 py-1 rounded-lg text-sm text-[#1a2838] border-[1px] border-[#b4c7db] hover:bg-[#a5cdff] hover:border-[#a5cdff] bg-[#D4E8FD]">Github</a>
