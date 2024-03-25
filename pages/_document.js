91cbf8ff4b8e9d0c1030fa6a0eb926d8a8e14e98
Anshuman2305 <anshuman.nitrr@gmail.com>
2024-03-25 14:38:24 +0530

    UI Update

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-[#f1f8ff] dark:bg-[#121212] ">
+      <body className="bg-[#f1f8ff] dark:bg-[#121212] ease-in-out duration-300">
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,28 @@ import Nav from '@/components/Nav'
 import Footer from '@/components/Footer'
 import Mainbody from '@/components/Mainbody'
 import { client } from '@/lib/sanity'
+import Head from 'next/head';
+
 
 
 function Home({ mcaProducts, gateProducts }) {
   return (
+    <>
+    <Head>
+        <title>Note Nest</title>
+        <meta
+          name="description"
+          content="Capturing Insights, One Note at a Time"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+  
+      </Head>
     <main className="flex flex-col items-center md:h-[100vh] max-w-[100vw] sm:max-w-[90vw] xl:max-w-[50vw] mx-auto sm:gap-4">
       <Nav />
       <Mainbody mcaProducts={mcaProducts} gateProducts={gateProducts}/>
       <Footer />
     </main>
+    </>
   )
 }
 
