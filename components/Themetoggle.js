e48869e637fb63fefb92fe499364ee274088d3cd
Anshuman2305 <anshuman.nitrr@gmail.com>
2024-03-25 09:47:53 +0530

    UI Update

--- components/Themetoggle.js
+++ components/Themetoggle.js
@@ -12,14 +12,14 @@ const ThemeToggler = () => {
     return (
         <>
             <button
-                className="pointer w-8 h-8 rounded-full flex items-center justify-center focus:outline-none col-span-1"
+                className="pointer min-w-8 min-h-8 rounded-full flex items-center justify-center focus:outline-none col-span-1"
                 onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
                 aria-label="Toggle Dark Mode"
             >
                 {theme === 'light' ? (
                     <MoonIcon className="bg-[#D4E8FD] border-[1px] border-[#b4c7db] text-[#1a2838] p-2 rounded-full" />
                 ) : (
-                    <SunIcon className="bg-[#242424] border-[1px] border-[#393939] text-[#fff] p-2 rounded-full" />
+                    <SunIcon className=" bg-[#242424] border-[1px] border-[#393939] text-[#fff] p-2 rounded-full" />
                 )}
             </button>
         </>
