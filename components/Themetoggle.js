f3d5ebf6d2a40b37607cf179a93e47d412a46d01
Anshuman2305 <anshuman.nitrr@gmail.com>
2024-03-25 10:19:33 +0530

    UI Update

--- components/Themetoggle.js
+++ components/Themetoggle.js
@@ -1,30 +1,53 @@
-import { useTheme } from 'next-themes';
-import { useState, useEffect } from 'react';
-import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
-
-
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 const ThemeToggler = () => {
-    const { theme, setTheme } = useTheme();
-    const [mounted, setMounted] = useState(false);
-    useEffect(() => setMounted(true), []);
-    if (!mounted) return null;
-    return (
-        <>
-            <button
-                className="pointer min-w-8 min-h-8 rounded-full flex items-center justify-center focus:outline-none col-span-1"
-                onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-                aria-label="Toggle Dark Mode"
-            >
-                {theme === 'light' ? (
-                    <MoonIcon className="bg-[#D4E8FD] border-[1px] border-[#b4c7db] text-[#1a2838] p-2 rounded-full" />
-                ) : (
-                    <SunIcon className=" bg-[#242424] border-[1px] border-[#393939] text-[#fff] p-2 rounded-full" />
-                )}
-            </button>
-        </>
-
-    );
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+  return (
+    <>
+      <button
+        className="pointer min-w-8 min-h-8 rounded-full flex items-center justify-center focus:outline-none col-span-1"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        aria-label="Toggle Dark Mode"
+      >
+        {theme === "light" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8 border-[1px] p-2 rounded-full border-[#b4c7db] bg-[#D4E8FD]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8 border-[1px] p-2 rounded-full border-[#393939] bg-[#242424]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+            />
+          </svg>
+        )}
+      </button>
+    </>
+  );
 };
 
 export default ThemeToggler;
